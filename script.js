7d32d2ad2b2a4a8dbcf80f1768a7e6c106ce3262
HigorLegal <146496157+HigorLegal@users.noreply.github.com>
2024-10-30 22:13:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 
-                    const radios = document.querySelectorAll('input[name="slider"]');
-                    let index = 0;
+  //pega uma lista de todos os inputs com o nome slider(no caso as coisas para mudar as imagens da tela)
+  const radios = document.querySelectorAll('input[name="slider"]');
+ 
+  //cria uma variavel com o valor 0 que ira ser usada para pegar a tela que voce deseja
+  let index = 0;
 
-                    //automatic slide change
-                    function changeSlide() {
-                        radios[index].checked = true; 
-                        index = (index + 1) % radios.length; 
-                    }
-                    setInterval(changeSlide, 3000);
+  //mudança automatica de slide
+  function changeSlide() {
+    
+    //ele vai checar se a tela esta sendo mostrada
+      radios[index].checked = true; 
+
+// ele vai adicionar 1 para o index vai dividir e pegar o resto
+      index = (index + 1) % radios.length; 
+  }
+
+  //seta um intervalo até que essa funçao aconteça
+  setInterval(changeSlide, 3000);
